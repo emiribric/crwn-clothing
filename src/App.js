fa4c2398a93e9b2f901d904f868480bc31a89302
Emir Ibric <emir.ibric@live.com>
2020-11-13 16:44:17 +0100

    MAking changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { /*Redirect,*/ Switch, Route } from 'react-router-dom';
+import { Redirect, Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
@@ -12,9 +12,11 @@ function App() {
     <div>
       <Header />      
       <Switch>
-        {/* <Route path="/crwn-clothing" exact>
-            <Redirect to="/" />
-        </Route> */}
+
+        <Route path="crwn-clothing/" exact>
+            <Redirect to="crwn-clothing" />
+        </Route>
+
         <Route exact path="/crwn-clothing" component={HomePage} />
         <Route exact path="/crwn-clothing/shop" component={ShopPage} />
       </Switch>     
